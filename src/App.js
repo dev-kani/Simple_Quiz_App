f3c2d3a62696d0d5f011964bf6d9fcc3f44f7fed
Kanishka Samarathunga <kanishkakcs@gmail.com>
2022-05-06 19:48:51 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -58,11 +58,8 @@ export default function App() {
 		}
 	}
 
-
 	return (
 		<div className='app'>
-			{/* HINT: replace "false" with logic to display the 
-      score when the user has answered all the questions */}
 			{showScore ? (
 				<div className='score-section'>You scored {score} out of {questions.length}</div>
 			) : (
